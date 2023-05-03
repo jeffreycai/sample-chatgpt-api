c417f11e0a6f5c909194f79f7021e1c087d3fb7a
Jeffrey Cai <jeffreycaizhenyuan@gmail.com>
2023-05-03 06:00:26 +0000

    Complete examle code

--- app/main.py
+++ app/main.py
@@ -4,14 +4,23 @@ import openai
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path='../.env')
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model="gpt-3.5-turbo", # gpt-4
     messages=[
+            # a system message helps to set the behavior of the assistant
             {"role": "system", "content": "You are a chatbot"},
-            {"role": "user", "content": "Why should DevOps engineer learn kubernetes?"},
+
+            # a user message helps to instruct the assistant
+            {"role": "user", "content": "Just say 'Hello world', nothing else"},
+
+            # an "assistant" message is what's returned, 
+            # and used when providing the overall context of the conversation
+            # when it's passed into the `messages` array in the request
+            {"role": "assistant", "content": "Hello world!"},
+
+            {"role": "user", "content": "What did you say just now?"},
         ]
 )
 
