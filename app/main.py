5823d9b87e7bfe0542625b9d1049f6fbbaa8ebd3
Zhen <zhenyuan.cai@justice.nsw.gov.au>
2023-05-05 13:16:40 +1000

    try gpt-4 api call

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ load_dotenv(dotenv_path='../.env')
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", # gpt-4
+    model="gpt-4", # gpt-4
     messages=[
             # a system message helps to set the behavior of the assistant
             {"role": "system", "content": "You are a chatbot"},
@@ -20,7 +20,7 @@ response = openai.ChatCompletion.create(
             # when it's passed into the `messages` array in the request
             {"role": "assistant", "content": "Hello world!"},
 
-            {"role": "user", "content": "What did you say just now?"},
+            {"role": "user", "content": "Are you gpt3 or gpt4?"},
         ]
 )
 
